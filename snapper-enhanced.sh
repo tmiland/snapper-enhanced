dcded0a6be9762bc61c28eafaa3183e6543b07a6
Tommy Miland <kontakt@tmiland.com>
2025-08-19 22:57:08 +0200

    Update snapper-enhanced version to 1.0.0

--- snapper-enhanced.sh
+++ snapper-enhanced.sh
@@ -1,5 +1,43 @@
 #!/usr/bin/env bash
 
+
+## Author: Tommy Miland (@tmiland) - Copyright (c) 2025
+
+
+######################################################################
+####                    Invidious Update.sh                       ####
+####            Automatic update script for Invidious             ####
+####            Script to update or install Invidious             ####
+####                   Maintained by @tmiland                     ####
+######################################################################
+
+VERSION='1.0.0' # Must stay on line 14 for updater to fetch the numbers
+
+#------------------------------------------------------------------------------#
+#
+# MIT License
+#
+# Copyright (c) 2025 Tommy Miland
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#------------------------------------------------------------------------------#
 # Set default snapshot description if above command fails
 SNAPPER_DESCRIPTION=${SNAPPER_DESCRIPTION:-"{SNAPPER_DESCRIPTION:-{snapper-enhanced} {created $1 apt command}"}
 snapper_apt_enhanced_tmp=/var/tmp/snapper-enhanced
